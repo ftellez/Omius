8e9b4e79d312b221aa02491de818fc8f46f9d926
ftellez <fernando.saisd@gmail.com>
2016-12-06 20:56:31 -0600

    PDF Reporte 06'12'16

--- graficaCorriente.m
+++ graficaCorriente.m
@@ -1,21 +1,21 @@
 %close all
 clear all
 
-fileID = fopen('pruebaIman2.txt','r');
-formatSpec = '%f,%f,%f\n';
-size = [3, Inf];
+fileID = fopen('test16_061216.txt','r');
+formatSpec = '%f,%f,%f,%f,%f\n';
+size = [5, Inf];
 
 A = fscanf(fileID,formatSpec,size);
 
 figure(1)
-%hold on
+hold on
 %subplot(2,1,1)
 plot(A(1,:)/1000,A(2,:))
 %plot(A(1,:)/1000,1000*A(5,:))
 %plot(A(1,:),(A(2,:)*10/255))
 %subplot(2,1,2)
-%plot(A(1,:)/1000,A(5,:))
+plot(A(1,:)/1000,A(5,:))
 figure(2)
-%hold on
+hold on
 plot(A(1,:)/1000,A(3,:))
-%plot(A(1,:)/1000,10*A(5,:))
+plot(A(1,:)/1000,10*A(5,:))
